bb60131ab2c6865b67d767a30107c2e00a24e559
guiguibassz <guilherme8galmeida@gmail.com>
2026-01-24 15:48:27 -0300

    Projeto inicial FilaTransparente

---
+++ filatransparente/src/main/java/br/com/filatransparente/FilatransparenteApplication.java
@@ -0,0 +1,13 @@
+package br.com.filatransparente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilatransparenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilatransparenteApplication.class, args);
+	}
+
+}
---
+++ filatransparente/src/test/java/br/com/filatransparente/FilatransparenteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.filatransparente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilatransparenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
